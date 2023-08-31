ea633c5dd76683b4abfbe420cf1511fb01d74ddd
Anderson <andersonfullstackdevoloper@gmail.com>
2023-08-31 00:11:19 -0300

    Projeto Realizado ate o 07.md

---
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,11 @@
+package com.generation.blogpessoal.repository;
+
+import com.generation.blogpessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostagemRepository extends JpaRepository<Postagem,Long> {
+
+
+
+
+}
