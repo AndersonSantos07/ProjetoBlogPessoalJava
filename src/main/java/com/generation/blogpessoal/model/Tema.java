c33632174ca4e972d9287580b17919338f66f5a7
Anderson <andersonfullstackdevoloper@gmail.com>
2023-09-01 09:27:45 -0300

    Projeto Feito até o 14.md

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,48 @@
+package com.generation.blogpessoal.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O Atributo Descrição é obrigatório")
+    private String descricao;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagem;
+
+    public List<Postagem> getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(List<Postagem> postagem) {
+        this.postagem = postagem;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal.repository;
+
+import com.generation.blogpessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TemaRepository extends JpaRepository<Tema,Long> {
+    public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+
+
+}
